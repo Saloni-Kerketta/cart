0d890b1e2e9071dd351d30e01d15bbc9a9520dff
Saloni Kerketta <salonikerketta48@gmail.com>
2025-07-24 11:20:29 +0530

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+//cartella
 export default {
   plugins: {
     tailwindcss: {},
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+//cartella
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
